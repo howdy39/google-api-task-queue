4bacf5f2edac5a035ec312a91490204bc4901676
howdy39 <tatsuya.howdy@gmail.com>
2017-07-16 21:56:23 +0900

    :sparkles: ログのサンプルコードを追加

--- src/main/java/jp/howdylikes/demo/HelloServlet.java
+++ src/main/java/jp/howdylikes/demo/HelloServlet.java
@@ -6,15 +6,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Logger;
 
 /**
  * Created by howdy39 on 2017/07/15.
  */
 public class HelloServlet extends HttpServlet {
 
+    private static final Logger log = Logger.getLogger(HelloServlet.class.getName());
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
         out.println("Hello, world!!!");
+
+        log.severe("severe");
+        log.warning("warning");
+        log.info("info");
+        log.fine("fine");
+        log.finer("finer");
+        log.finest("finest");
     }
 }
